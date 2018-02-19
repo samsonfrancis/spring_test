2ee820495a8bf277df5a0a99bfca58858df44ee8
samsonfrancis <samson@kritter.in>
2018-02-19 16:22:30 +0530

    add init and destroy for bean

--- src/main/java/com/sam/DrawingApp.java
+++ src/main/java/com/sam/DrawingApp.java
@@ -1,16 +1,14 @@
 package com.sam;
 
 import org.springframework.beans.factory.BeanFactory;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
-public class DrawingApp
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Inside Main" );
-        AbstractApplicationContext context=new ClassPathXmlApplicationContext(new String[]{"src/main/resources/spring.xml"});
+public class DrawingApp {
+    public static void main(String[] args) {
+        System.out.println("Inside Main");
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"src/main/resources/spring.xml"});
         context.registerShutdownHook();// this destroys the context when it exits this main method - note previous line is changed to AbstractApplicationContext instead of ApplicationContext
         BeanFactory beanFactory = context;
         Triangle triangle = (Triangle) beanFactory.getBean("triangle1");
--- src/main/java/com/sam/Triangle.java
+++ src/main/java/com/sam/Triangle.java
@@ -2,8 +2,10 @@ package com.sam;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 
-public class Triangle {
+public class Triangle implements InitializingBean, DisposableBean{
     @Getter@Setter
     private Point a;
     @Getter@Setter
@@ -14,5 +16,22 @@ public class Triangle {
         System.out.println(getA().getX() + "," + getA().getY() );
         System.out.println(getB().getX() + "," + getB().getY() );
         System.out.println(getC().getX() + "," + getC().getY() );
+    }
+
+    /*
+     * this method is called when creating the bean after initializing the variables
+     */
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        // i can write the piece of code that need to be executed after initiazing the here.
+        System.out.println("Inside Initializing method of Triangle bean");
+    }
+
+    /*
+     * this method is called just before the bean is destroyed
+     */
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("Inside destory method of Traiangle bean.");
     }
 }
