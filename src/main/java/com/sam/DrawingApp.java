847949cef207f4552bb3938755beac8a5637600c
samsonfrancis <samson@kritter.in>
2018-02-19 16:10:44 +0530

    add context destroy

--- src/main/java/com/sam/DrawingApp.java
+++ src/main/java/com/sam/DrawingApp.java
@@ -10,7 +10,8 @@ public class DrawingApp
     public static void main( String[] args )
     {
         System.out.println( "Inside Main" );
-        ApplicationContext context=new ClassPathXmlApplicationContext(new String[]{"src/main/resources/spring.xml"});
+        AbstractApplicationContext context=new ClassPathXmlApplicationContext(new String[]{"src/main/resources/spring.xml"});
+        context.registerShutdownHook();// this destroys the context when it exits this main method - note previous line is changed to AbstractApplicationContext instead of ApplicationContext
         BeanFactory beanFactory = context;
         Triangle triangle = (Triangle) beanFactory.getBean("triangle1");
         triangle.draw();
